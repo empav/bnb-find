69d0d130084945ef84da6f80c169406993c9765a
Emanuele Pavan <emanuele.pav@gmail.com>
2023-06-20 15:42:40 +0200

    implements rent modal (categories step)

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -9,6 +9,7 @@ import useLoginModal from '@/app/hooks/useLoginModal';
 
 import { User } from '@prisma/client';
 import { signOut } from 'next-auth/react';
+import useRentModal from '@/app/hooks/useRentModal';
 
 type UserMenuProps = {
   currentUser?: User | null;
@@ -17,6 +18,8 @@ type UserMenuProps = {
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
+  const rentModal = useRentModal();
+
   const [isOpen, setIsOpen] = useState<Boolean>(false);
 
   const toggleOpen = useCallback(() => {
@@ -33,15 +36,22 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
     loginModal.onOpen();
   }, [loginModal, toggleOpen]);
 
+  const handleRent = useCallback(() => {
+    toggleOpen();
+    rentModal.onOpen();
+  }, [rentModal, toggleOpen]);
+
   return (
     <div className='relative'>
       <div className='flex items-center gap-3'>
-        <div
-          className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'
-          onClick={() => {}}
-        >
-          B&B your home
-        </div>
+        {currentUser && (
+          <div
+            className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'
+            onClick={handleRent}
+          >
+            BnB my home
+          </div>
+        )}
         <div
           onClick={toggleOpen}
           className='p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'
@@ -52,7 +62,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
           </div>
         </div>
         {isOpen && (
-          <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
+          <div className='absolute rounded-xl shadow-md w-[10vw] bg-white overflow-hidden right-0 top-12 text-sm'>
             <div className='flex flex-col cursor-pointer'>
               {currentUser ? (
                 <>
@@ -60,7 +70,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                   <MenuItem label='My favorites' onClick={() => {}} />
                   <MenuItem label='My reservations' onClick={() => {}} />
                   <MenuItem label='My properties' onClick={() => {}} />
-                  <MenuItem label='Airbnb your home' onClick={() => {}} />
+                  <MenuItem label='BnB my home' onClick={handleRent} />
                   <hr />
                   <MenuItem label='Logout' onClick={signOut} />
                 </>
