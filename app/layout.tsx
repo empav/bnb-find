35412f873aa10da63af6d2e248faf8c7c9565933
Emanuele Pavan <emanuele.pav@gmail.com>
2023-06-23 18:29:06 +0200

    implements search

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import LoginModal from './components/modals/LoginModal';
 import ToastProvider from './providers/ToastProvider';
 import getCurrentUser from './actions/getCurrentUser';
 import RentModal from './components/modals/RentModal';
+import SearchModal from './components/modals/SearchModal';
 
 const font = Nunito({ subsets: ['latin'] });
 
@@ -29,6 +30,7 @@ export default async function RootLayout({
         <RegisterModal />
         <LoginModal />
         <RentModal />
+        <SearchModal />
         <main className='pt-40 pb-20'>{children}</main>
       </body>
     </html>
